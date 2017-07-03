0c60273f197579192e4edf4ca04925bb2ae1bfbb
Tim Qi <i@timqi.com>
2017-07-03 23:29:27 +0800

    update

--- install.sh
+++ install.sh
@@ -3,3 +3,9 @@
 cd ~
 ln -s -f .tmux/tmux.conf .tmux.conf
 
+# TPM plugin manager
+# Then <prefix> + I (Uppercase) to INSTALL
+# 
+# <prefix> + (<Alt> + u (Lowercase)) [ OR comment out in tmux.conf ] to Uninstall
+# <prefix> + U (Uppercase) to Update
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
